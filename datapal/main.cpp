46a3343fd5786ffcf0f84593cdfaf2a284c8f0e2
MorariuT <59195041+MorariuT@users.noreply.github.com>
2020-05-09 13:11:29 +0300

    Add files via upload

--- datapal/main.cpp
+++ datapal/main.cpp
@@ -5,55 +5,34 @@ ifstream fin("datapal.in");
 ofstream fout("datapal.out");
 int main()
 {
-    int n = 0;
-    int n2 = 0;
-    int i = 1;
-    int c1,c2,c3,c4,c5,c6,c7,c8;
-    int max_sec = 0;
-    int l_num[100];
-    int h = 0;
-    while(h < 100)
-    {
-        l_num[h] = 0;
-        h++;
-    }
+    int n = 0; fin >> n;
+    char v[8];
     int num = 0;
-    int nr = 0;
-    int am_gasit = 0;
-    fin >> n;
-    while(i <= n)
+    char x = 10;
+    int termen1 = 0;
+    int termen2 = 0;
+    int secole[101];
+    for(int i = 0;i < 101;i++) secole[i] = 0;
+    for(int i = 0;i < n;i++)
     {
-        fin >> n2;
-        int raspuns = 0;
-        c1 = (n2/10000000)%10;
-        c2 = (n2/1000000)%10;
-        c3 = (n2/100000)%10;
-        c4 = (n2/10000)%10;
-        c5 = (n2/1000)%10;
-        c6 = (n2/100)%10;
-        c7 = (n2/10)%10;
-        c8 = n2%10;
-        if(c1 == c8 && c2 == c7 && c3 == c6 && c4 == c5)
+        fin >> v[0] >> v[1] >> v[2] >> v[3] >> v[4] >> v[5] >> v[6] >> v[7];
+        termen1 = v[4]-'0';
+        termen2 = v[5]-'0';
+        //cout << secol << endl;
+        if(v[0] == v[7] && v[1] == v[6] && v[2] == v[5] && v[3] == v[4])
         {
             num++;
-            nr = c5*10+(c6+1);
-            l_num[nr]++;
+            int secol = termen1*10+termen2+1;
+            secole[secol]++;
         }
-        i++;
     }
-    int y = 0;
-    while(y < 100)
-    {
-        if(l_num[y] > max_sec) max_sec = l_num[y];
-        y++;
-    }
-    int q = 0;
     fout << num << endl;
-    while(q < 100)
+    for(int i = 0;i < 101;i++)
     {
-        if(l_num[q] == max_sec) fout << q << " ";
-        q++;
-
+        if(secole[i] > 0)
+        {
+            fout << i << " ";
+        }
     }
     return 0;
 }
